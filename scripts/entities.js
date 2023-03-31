7e9af3db773f999aa1c4f4326292e53770333163
alfre <alfredo.medina@uc.cl>
2023-03-31 00:51:00 -0300

    ft: enemy

--- scripts/entities.js
+++ scripts/entities.js
@@ -8,15 +8,7 @@ class Entity {
         this.direction = "up";
         this.speed = 1;
     }
-}
-
-
-class Player extends Entity {
-    constructor(id, x, y, size, keymap) {
-        super(id, x, y, size);
-        this.keymap = keymap;
-    }
-
+    
     move(direction) {
         switch (direction) {
             case "up":
@@ -34,9 +26,66 @@ class Player extends Entity {
         }
     }
 
-
     changeDirection(direction) {
         this.direction = direction;
     }
+
+    toString() {
+        return `${this.id}: (${this.x}, ${this.y})`;
+    }
 }
 
+
+class Player extends Entity {
+    constructor(id, x, y, size, keymap) {
+        super(id, x, y, size);
+        this.keymap = keymap;
+    }
+}
+
+
+class Enemy extends Entity {
+    constructor(id, x, y, size) {
+        super(id, x, y, size);
+    }
+
+    chooseDirection(canvas) {
+        const surroundings = this.getSurroundings(canvas);
+        let validSurroundings = surroundings.filter((surrounding) => {
+            const key = Object.keys(surrounding)[0];
+            const value = surrounding[key];
+            return value !== '#';
+        });
+        if (validSurroundings.length === 1) return Object.keys(validSurroundings[0])[0];
+        validSurroundings = validSurroundings.filter((surrounding) => {
+            const key = Object.keys(surrounding)[0];
+            return key !== this.opositeDirection(this.direction);
+        });
+        const randomIndex = Math.floor(Math.random() * validSurroundings.length);
+        const randomCell = validSurroundings[randomIndex];
+        return Object.keys(randomCell)[0];
+    }
+
+    opositeDirection(direction) {
+        switch (direction) {
+            case "up":
+                return "down";
+            case "down":
+                return "up";
+            case "left":
+                return "right";
+            case "right":
+                return "left";
+        }
+    }
+
+    getSurroundings(canvas) {
+        const surroundings = [];
+        surroundings.push({"up" : canvas.getCell(this.x, this.y - 1)});
+        surroundings.push({"down" : canvas.getCell(this.x, this.y + 1)});
+        surroundings.push({"left" : canvas.getCell(this.x - 1, this.y)});
+        surroundings.push({"right" : canvas.getCell(this.x + 1, this.y)});
+        return surroundings;
+    }
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -41,8 +41,7 @@ const main = () => {
                 return player;
             }, playerObj);
 
-
-    const suscribePlayer = (observablePlayer, playerObj, canvas) => 
+    const suscribeEntity = (observablePlayer, playerObj, canvas) => 
         observablePlayer.subscribe((player) => {
             if (canvas.getNextPlayerCell(player) === '#') return;
             canvas.clearPlayer(playerObj);
@@ -56,13 +55,26 @@ const main = () => {
     const player1 = new Player("player1", x = 1, y = 1, size = sizeCharacter, KEYMAP.player1);
     canvas.drawPlayer(player1);
     const player1$ = changeDirectionPlayer(player1);
-    suscribePlayer(player1$, player1, canvas);
+    suscribeEntity(player1$, player1, canvas);
 
-//     // player 2
-//     const player2 = new Player("player2", x = 2, y = 1, size = sizeCharacter, KEYMAP.player2);
-//     canvas.drawPlayer(player2);
-//     const player2$ = changeDirectionPlayer(player2);
-//     suscribePlayer(player2$, player2, canvas);
+    // // player 2
+    // const player2 = new Player("player2", x = 2, y = 1, size = sizeCharacter, KEYMAP.player2);
+    // canvas.drawPlayer(player2);
+    // const player2$ = changeDirectionPlayer(player2);
+    // suscribePlayer(player2$, player2, canvas);
+
+    const moveEnemy = (enemy, canvas) => Rx.Observable.interval(300)
+        .scan((enemy) => {
+            const direction = enemy.chooseDirection(canvas);
+            enemy.changeDirection(direction);
+            return enemy;
+        }, enemy);
+
+    // enemy 1
+    const enemy1 = new Enemy("enemy1", x = 12, y = 10, size = sizeCharacter);
+    canvas.drawPlayer(enemy1);
+    const enemy1$ = moveEnemy(enemy1, canvas);
+    suscribeEntity(enemy1$, enemy1, canvas);
 }
 
 main();
