439ce12d24a19aa9c654a18752916913f841d4c3
alfre <alfredo.medina@uc.cl>
2023-03-31 16:49:53 -0300

    unsubscribe on close window

--- scripts/main.js
+++ scripts/main.js
@@ -34,6 +34,8 @@ const KEYMAP = {
 };
 
 const sizeCell = 100;
+var entities = [];
+
 
 
 const main = () => {
@@ -44,22 +46,44 @@ const main = () => {
     // player 1
     const sizeCharacter = sizeCell;
     const player1 = new Player("player1", x = 1, y = 1, size = sizeCharacter, KEYMAP.player1);
+    entities.push(player1);
+
     canvas.drawEntity(player1);
     player1.declareObservablePlayer();
     player1.suscribeEntity(canvas);
 
     // // player 2
     // const player2 = new Player("player2", x = 1, y = 1, size = sizeCharacter, KEYMAP.player2);
+    // entities.push(player2);
     // canvas.drawEntity(player2);
     // player2.declareObservablePlayer();
     // player2.suscribeEntity(canvas);
 
     // enemy 1
     const enemy1 = new Enemy("enemy1", x = 12, y = 10, size = sizeCharacter);
+    entities.push(enemy1);
     canvas.drawEntity(enemy1);
     enemy1.declareObservableEnemy(canvas);
     enemy1.suscribeEntity(canvas);
-
 }
 
+
 main();
+
+
+////////////////////////////////////////////////////////////////////
+// IMPORTANT !!
+// unsubscribe all entities when close window to avoid memory leaks
+
+const unsubscribeAll = (arrayEntities) => {
+    arrayEntities.forEach((entity) => {
+        entity.unsuscribeEntity();
+    });
+    console.log("All entities unsubscribed");
+}
+
+window.addEventListener('beforeunload', (event) => {
+    unsubscribeAll(entities);
+});
+
+////////////////////////////////////////////////////////////////////
